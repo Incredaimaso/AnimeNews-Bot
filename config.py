8bda120d73240e401e24f6c1652d202bee9848b4
Incredaimaso <159280005+Incredaimaso@users.noreply.github.com>
2025-03-19 14:27:02 +0530

    Update config.py

--- config.py
+++ config.py
@@ -3,8 +3,11 @@
 API_ID = "" #API ID get it from telegram.org
 API_HASH = "" #API HASH get it from telegram.org
 BOT_TOKEN = "" #Bot Token get it from @BotFather
-URL_A = "" #News RSS Feed Url 2
-#URL_B = "" #News RSS Feed Url 2
+NEWS_FEED_URLS = [
+    "https://example.com/rss_feed_1",
+    "https://example.com/rss_feed_2"
+]
+
 START_PIC = "" #Startup Pic
 MONGO_URI = "" #Databse uri get it from https://www.mongodb.com/
 STICKER_ID = "" # Replace with your sticker's file ID
